3a961c323405318684863c57d2a07f468a88158c
Ann <ann.mitrakhovich@gmail.com>
2023-06-30 13:30:58 +0300

    Task 47 done

--- Program.cs
+++ Program.cs
@@ -4,6 +4,39 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+double[,] Create2DArray(int rows, int cols)
+{
+    return new double[rows, cols];
+}
+void Fill2DArray(double[,] array, int min, int max)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = Math.Round(rnd.Next(min, max + 1) * 0.1, 1);
+}
+void Print2DArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]}\t");
+        Console.WriteLine();
+    }
+}
+int rows = InputNum("Введите количество строк: ");
+int columns = InputNum("Введите количество столбцов: ");
+int minN = InputNum("Введите минимальное значение в диапазоне чисел массива: ");
+int maxN = InputNum("Введите максимальное значение в диапазоне чисел массива: ");
+double[,] myArray = Create2DArray(rows, columns);
+Fill2DArray(myArray, minN, maxN);
+Print2DArray(myArray);
 
 // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 // Например, задан массив:
